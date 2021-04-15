b273f9c770c39faf41ebfde50544614597d2ca4a
mustafatokmak <mtokmak06@gmail.com>
2021-04-15 05:18:16 +0300

    required fields validations taken from frontend to backend

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    validates :category_name, presence: true
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
+	validates :currency, :first_name, :last_name, :gross_amount, presence: true
+
 	validate :validate_discount_amount
-	
+
   def validate_discount_amount
     if discount_amount > gross_amount
       errors.add(:discount_amount, "can not be bigger than gross amount")
